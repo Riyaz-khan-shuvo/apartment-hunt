8e31008ddcbbe881497dc5eb0792dbdb1fcdbe1e
rafsan <rafsanamin18@gmail.com>
2020-11-16 11:26:10 +0600

    second commit

--- src/components/BookSingleItem/BookSingleItem.js
+++ src/components/BookSingleItem/BookSingleItem.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const BookSingleItem = ({ item }) => {
     const { name, email, phone, message, status } = item;
+
     return (
         <tr>
             <td> {name} </td>
